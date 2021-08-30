2026d762e316ad55c9915d44857f17b2a8a88288
Nvard Asatryan <nvard.asatryan96@gmail.com>
2021-08-30 18:29:22 +0400

    asas

--- js/script.js
+++ js/script.js
@@ -50,7 +50,13 @@ if($(window).innerWidth() >= 1100) {
 //onclick="document.getElementById('id01').style.display='block'"
 $('#register').click(function(){
     $('#id01').show();
-})
+});
+$('#adpro').click(function(){
+    $('#id02').show();
+});
+$('#login').click(function(){
+    $('#id03').show();
+});
 $('.customer').click(function(){
 
     $('#mod-1').hide();
@@ -60,5 +66,35 @@ $('.customer').click(function(){
 $('#email').click(function(){
     $('#mod-2').hide();
     $('#mod-3').show();
+
+})
+$('#add-account').click(function(){
+    $('#mod-3').hide();
+    $('#mod-4').show();
+
+});
+$('#nxt').click(function(){
+    $('#mod-4').hide();
+    $('#mod-5').show();
+
+});
+$('#send-part').click(function(){
+    $('#mod-5').hide();
+    $('#mod-6').show();
+
+})
+$('#confirm').click(function(){
+    $('#mod-6').hide();
+    $('#mod-7').show();
+
+})
+$('#cl-cod').click(function(){
+    $('#mod-7').hide();
+    $('#mod-8').show();
+
+});
+$('#chg').click(function(){
+    $('#mod-8').hide();
+    $('#mod-9').show();
 
 })
